84c004e3427b09e93f6948904a528784044ada91
Marijn Pool <info@marijnpool.nl>
2015-09-25 22:35:55 +0200

    Added SerialVersionUID

--- src/weatherhandler/processor/ProcessorException.java
+++ src/weatherhandler/processor/ProcessorException.java
@@ -3,6 +3,11 @@ package weatherhandler.processor;
 import java.lang.Exception;
 
 public class ProcessorException extends Exception {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
     public ProcessorException(Exception e) {
         super("Processor exception:", e);
     }
